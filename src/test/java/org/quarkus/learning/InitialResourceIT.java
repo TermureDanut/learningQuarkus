3b677e24a180576929a07f02998a38c52bab1a4e
Termure Danut <termure.danut@ibm.com>
2024-02-01 14:09:53 +0200

    Setting up Quarkus project

---
+++ src/test/java/org/quarkus/learning/InitialResourceIT.java
@@ -0,0 +1,8 @@
+package org.quarkus.learning;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class InitialResourceIT extends InitialResourceTest {
+    // Execute the same tests but in packaged mode.
+}
