2a5da671359269e8543c97a3d83929bbe40452cb
TermureDanut <danutvaleriu.termure@gmail.com>
2024-02-01 16:41:20 +0200

    Deleted unnecessary tests

--- src/test/java/org/quarkus/learning/InitialResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.quarkus.learning;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class InitialResourceIT extends InitialResourceTest {
-    // Execute the same tests but in packaged mode.
-}
